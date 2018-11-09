4b3a3930d58a9ac38cef17f55c2fd131fd9abcaa
Rahul Chowdhury <rahul@mitter.io>
2018-11-09 18:41:43 +0530

    Add Swinject

---
+++ Mitter/Mitter/Domain/Base/UniParamAction.swift
@@ -0,0 +1,14 @@
+//
+//  UniParamAction.swift
+//  Mitter
+//
+//  Created by Rahul Chowdhury on 09/11/18.
+//  Copyright © 2018 Chronosphere Technologies Pvt. Ltd. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+protocol UniParamAction {
+    <#requirements#>
+}
