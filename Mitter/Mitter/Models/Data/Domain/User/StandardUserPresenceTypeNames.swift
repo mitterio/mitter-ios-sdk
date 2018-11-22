dff62b80dffb703967a661444aab285afa9b667f
Rahul Chowdhury <rahul@mitter.io>
2018-11-22 15:56:37 +0530

    Add presence model

---
+++ Mitter/Mitter/Models/Data/Domain/User/StandardUserPresenceTypeNames.swift
@@ -0,0 +1,17 @@
+//
+//  StandardUserPresenceTypeNames.swift
+//  Mitter
+//
+//  Created by Rahul Chowdhury on 22/11/18.
+//  Copyright © 2018 Chronosphere Technologies Pvt. Ltd. All rights reserved.
+//
+
+import Foundation
+
+public enum StandardUserPresenceTypeNames {
+    static let Online: String = "mitter.up.Online"
+    static let Sleeping: String = "mitter.up.Sleeping"
+    static let Away: String = "mitter.up.Away"
+    static let Missing: String = "mitter.up.Missing"
+    static let Offline: String = "mitter.up.Offline"
+}
